bf31f91944a51203fbb72890d683b464592fd5bb
t <user@example.com>
2023-10-22 19:44:51 -0700

    updated

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Ranger terminal file manager custom code
-This code is for the terminal Vim based file manager Ranger
+This code is for the terminal Vim based file manager Ranger. Must have Ranger installed of course.
+
 https://github.com/ranger/ranger
+
+Copy contents from `commands.py` & `rc.conf` to the end of the respective files on your local Ranger installation.
+Every class in the commands.py file is a separate functionality, so you can delete customize or delete to you liking. 
+
+The rc.conf file are shortcut keybinds, those can be edited to your liking as well.
+
+For the `runbuild` class/functionality to work, you'll need to set the paths marked with "/path/to/python/binary" in the both the class and contents in the `build` folder and terminal accordingly. 
+
--- commands.py
+++ commands.py
@@ -61,7 +61,9 @@ class token_my_edit(Command):
         # content of the current directory.
         return self._tab_directory_content()
 
-########### =========================== MY Custom Commands ===================================
+# =========================== MY Custom Commands ===================================
+
+
 class vscode(Command):
     def execute(self):
         if self.fm.thisdir.marked_items:
@@ -71,6 +73,7 @@ class vscode(Command):
         else:
             target_filename = self.fm.thisfile.path
             self.fm.execute_command(f"code '{target_filename}'")
+
     def tab(self, tabnum):
         return self._tab_directory_content()
 
@@ -78,6 +81,7 @@ class vscode(Command):
 class gk(Command):
     def execute(self):
         self.fm.execute_command('guake -n $(pwd) --show')
+
     def tab(self, tabnum):
         return self._tab_directory_content()
 
@@ -85,26 +89,35 @@ class gk(Command):
 class dol(Command):
     def execute(self):
         self.fm.execute_command('dolphin --new-window . &> /dev/null & disown')
+
     def tab(self, tabnum):
         return self._tab_directory_content()
+
 
 class k4(Command):
     def execute(self):
         self.fm.execute_command('k4dirstat . &> /dev/null & disown')
+
     def tab(self, tabnum):
         return self._tab_directory_content()
+
 
 class kon(Command):
     def execute(self):
         self.fm.execute_command('konsole . &> /dev/null & disown')
+
     def tab(self, tabnum):
         return self._tab_directory_content()
 
+
 class sublimeDir(Command):
     def execute(self):
-        self.fm.execute_command(f'/home/t/stuff/sublime_text/sublime_text . -n &> /dev/null & disown')
+        self.fm.execute_command(
+            f'sublime_text . -n &> /dev/null & disown')
+
     def tab(self, tabnum):
         return self._tab_directory_content()
+
 
 class sublimeSingle(Command):
     def execute(self):
@@ -113,19 +126,24 @@ class sublimeSingle(Command):
             selection = [f.path for f in self.fm.thistab.get_selection()]
             for file in selection:
                 sel_list.append(file)
-            self.fm.execute_command(f'''/home/t/stuff/sublime_text/sublime_text {' '.join(f"'{w}'" for w in sel_list)} -n''')
+            self.fm.execute_command(
+                f'''sublime_text {' '.join(f"'{w}'" for w in sel_list)} -n''')
         else:
             target_filename = self.fm.thisfile.path
         if not os.path.exists(target_filename):
             self.fm.notify("The given file does not exist!", bad=True)
             return
-        self.fm.execute_command(f"/home/t/stuff/sublime_text/sublime_text '{target_filename}' -n")
+        self.fm.execute_command(
+            f"sublime_text '{target_filename}' -n")
+
     def tab(self, tabnum):
         return self._tab_directory_content()
+
 
 class vscodedir(Command):
     def execute(self):
         self.fm.execute_command('code . -n &> /dev/null & disown')
+
     def tab(self, tabnum):
         return self._tab_directory_content()
 
@@ -133,6 +151,7 @@ class vscodedir(Command):
 class bash(Command):
     def execute(self):
         self.fm.execute_command('bash')
+
     def tab(self, tabnum):
         return self._tab_directory_content()
 
@@ -142,19 +161,24 @@ class git(Command):
         if self.arg(1):
             url = self.rest(1)
             repo_name = url.replace('.git', '/').split('/')[-2]
-            self.fm.execute_command(f"git clone {url}") 
+            self.fm.execute_command(f"git clone {url}")
             self.fm.cd(repo_name)
             self.fm.execute_command(f"rm -rf .git")
             self.fm.execute_command(f"echo {url} > ORIG_REPO_URL.txt")
+
     def tab(self, tabnum):
         return self._tab_directory_content()
+
 
 class ln(Command):
     def execute(self):
         self.fm.notify(f"'{self.fm.thisfile}'")
-        self.fm.execute_command(f"ln -s {self.fm.thisfile} /home/t/Desktop/paths")
+        self.fm.execute_command(
+            f"ln -s {self.fm.thisfile} /home/user/Desktop/paths")
+
     def tab(self, tabnum):
         return self._tab_directory_content()
+
 
 class copy_file_content(Command):
     def execute(self):
@@ -164,8 +188,10 @@ class copy_file_content(Command):
         else:
             self.fm.execute_command(f"xclip -sel c < '{self.fm.thisfile}'")
             self.fm.notify(f"'{self.fm.thisfile}' copied")
+
     def tab(self, tabnum):
         return self._tab_directory_content()
+
 
 class paste_file_content(Command):
     def execute(self):
@@ -175,8 +201,10 @@ class paste_file_content(Command):
         else:
             self.fm.execute_command(f"xsel -b > '{self.fm.thisfile}'")
             self.fm.notify(f"'{self.fm.thisfile}' pasted")
+
     def tab(self, tabnum):
         return self._tab_directory_content()
+
 
 class append_file_content(Command):
     def execute(self):
@@ -186,34 +214,40 @@ class append_file_content(Command):
         else:
             self.fm.execute_command(f"xsel -b >> '{self.fm.thisfile}'")
             self.fm.notify(f"'{self.fm.thisfile}' pasted")
+
     def tab(self, tabnum):
         return self._tab_directory_content()
+
 
 class open_nvim(Command):
     def execute(self):
         self.fm.execute_command('nvim')
+
     def tab(self, tabnum):
         return self._tab_directory_content()
+
 
 class vim_rename(Command):
     def execute(self):
         import random
         import string
-        chars = string.ascii_letters + string.digits 
+        chars = string.ascii_letters + string.digits
         r_string = ''.join([random.choice(chars) for x in range(15)])
         if len(self.fm.thisdir.marked_items) > 1:
             self.fm.notify(f'only 1 file per')
         else:
-             old_name = self.fm.thisfile.basename
-             new_name = f"temp_rename_{r_string}"
-             self.fm.execute_command(f"echo '{old_name}' > {new_name}")
-             self.fm.execute_command(f"nvim {new_name}")
-             with open(new_name, 'r') as f:
+            old_name = self.fm.thisfile.basename
+            new_name = f"temp_rename_{r_string}"
+            self.fm.execute_command(f"echo '{old_name}' > {new_name}")
+            self.fm.execute_command(f"nvim {new_name}")
+            with open(new_name, 'r') as f:
                 content = f.read().split('\n')[0]
                 self.fm.rename(self.fm.thisfile, content)
-             self.fm.execute_command(f"rm {self.fm.thisdir}/{new_name}")
+            self.fm.execute_command(f"rm {self.fm.thisdir}/{new_name}")
+
     def tab(self, tabnum):
         return self._tab_directory_content()
+
 
 class runbuild(Command):
     def execute(self):
@@ -228,12 +262,15 @@ class runbuild(Command):
             self.runfile(ext, file)
 
     def runfile(self, ext, file):
-        if ext=='py':
-            self.fm.execute_command(f"path/to/builds/runpy.sh {file} &> /dev/null & disown")
-        if ext=='js': 
-            self.fm.execute_command(f"path/to/builds/runjs.sh {file} &> /dev/null & disown")
-        if ext=='go': 
-            self.fm.execute_command(f"path/to/builds/rungo.sh {file} &> /dev/null & disown")
+        if ext == 'py':
+            self.fm.execute_command(
+                f"path/to/builds/runpy.sh {file} &> /dev/null & disown")
+        if ext == 'js':
+            self.fm.execute_command(
+                f"path/to/builds/runjs.sh {file} &> /dev/null & disown")
+        if ext == 'go':
+            self.fm.execute_command(
+                f"path/to/builds/rungo.sh {file} &> /dev/null & disown")
 
     def tab(self, tabnum):
         return self._tab_directory_content()
@@ -244,12 +281,15 @@ class yank_realpath(Command):
         real_p = f"realpath {self.fm.thisfile}"
         self.fm.execute_command(f"{real_p} | xclip -sel clip")
         self.fm.notify(f"{real_p} copied")
+
     def tab(self, tabnum):
         return self._tab_directory_content()
 
+
 class ranger_here(Command):
     def execute(self):
-        self.fm.execute_command(f"xfce4-terminal --hold -x ranger . & /dev/null & disown")
+        self.fm.execute_command(
+            f"xfce4-terminal --hold -x ranger . & /dev/null & disown")
 
     def tab(self, tabnum):
         return self._tab_directory_content()
@@ -258,7 +298,9 @@ class ranger_here(Command):
 class shutter_edit(Command):
     def execute(self):
         file = self.fm.thisfile
-        self.fm.execute_command(f"""xfce4-terminal -e '''bash -c "shutter {file}"'''""")
+        self.fm.execute_command(
+            f"""xfce4-terminal -e '''bash -c "shutter {file}"'''""")
+
     def tab(self, tabnum):
         return self._tab_directory_content()
 
@@ -270,8 +312,10 @@ class animated_gif_mp4(Command):
         self.fm.execute_command(f'')
         # self.fm.execute_command(f"""xfce4-terminal -e '''bash -c "shutter {file}"'''""")
         # self.fm.notify(file)
+
     def tab(self, tabnum):
         return self._tab_directory_content()
+
 
 class trashcli(Command):
     def execute(self):
@@ -282,6 +326,7 @@ class trashcli(Command):
         else:
             target_filename = self.fm.thisfile.path
             self.fm.execute_command(f"trash-put '{target_filename}'")
+
     def tab(self, tabnum):
         return self._tab_directory_content()
 
@@ -289,6 +334,7 @@ class trashcli(Command):
 class gwenview_here(Command):
     def execute(self):
         self.fm.execute_command('gwenview . &> /dev/null & disown')
+
     def tab(self, tabnum):
         return self._tab_directory_content()
 
@@ -301,39 +347,38 @@ class gwenview_here(Command):
 class kk(Command):
     def execute(self):
         self.fm.execute_command(f"./path/to/test.sh {self.fm.thisfile}")
+
     def tab(self, tabnum):
         return self._tab_directory_content()
+
 
 class simple_execute(Command):
     def execute(self):
         self.fm.execute_command('guake -n $(pwd) --show')
+
     def tab(self, tabnum):
         return self._tab_directory_content()
 
-############## ---------NOTES--------------------------
+# ---------NOTES--------------------------
 
 
 # self.fm.thisfile.basename
 # --- returns just the basename of file (something.txt)
 
-#self.fm.thisfile
-#--- return the path + file name. full path to file
+# self.fm.thisfile
+# --- return the path + file name. full path to file
 
 
-#self.fm.thisfile.path
+# self.fm.thisfile.path
 # --- returns same as thisfile, full path to file
 
-#self.fm.<command>(args)
+# self.fm.<command>(args)
 # --- executes an existing command in this file/Commands
 
 
-#self.fm.open_console
+# self.fm.open_console
 # --- open string in console but doesnt execute
 
 
-# self.fm.execute_console(string): 
+# self.fm.execute_console(string):
 # --- Execute the string as a ranger command.
-
-
-
-
